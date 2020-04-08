8b92522ac46ca4285c86d46545dcff818b23a1a1
Abhishekraj166 <abhishek.raj166@gmail.com>
2020-04-08 00:25:32 -0500

    localization of robot

---
+++ Localization of robot/build/ball_chaser/CMakeFiles/bot_driver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bot_driver.dir/src/drive_bot.cpp.o"
+  "/home/workspace/catkin_ws/devel/lib/ball_chaser/bot_driver.pdb"
+  "/home/workspace/catkin_ws/devel/lib/ball_chaser/bot_driver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bot_driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
